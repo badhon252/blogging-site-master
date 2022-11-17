e06fac2a689df1d9fbf6949ecf62f98f4fa06db3
khalid <dev.khalidhossain@gmail.com>
2022-11-17 17:29:43 +0600

    there is some error in "firebase.js" file

---
+++ .history/public/js/firebase_20221117141710.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "./firebase/app";
+import { getFirestore } from "./firebase/firestore/lite";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDLHITpeUbkPkEOfpvXdKr1kGeCO33-kaI",
+
+  authDomain: "desi-blogging-website.firebaseapp.com",
+
+  projectId: "desi-blogging-website",
+
+  storageBucket: "desi-blogging-website.appspot.com",
+
+  messagingSenderId: "927647131525",
+
+  appId: "1:927647131525:web:e5feafc02dbbce7a9086ec",
+
+  measurementId: "G-YS4ZCDKKN1",
+};
+
+// firebase.initializeApp(firebaseConfig);
+// export const db = getFirestore("blogs");
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+// async function getCities(db) {
+//   const citiesCol = collection(db, "cities");
+//   const citySnapshot = await getDocs(citiesCol);
+//   const cityList = citySnapshot.docs.map((doc) => doc.data());
+//   return cityList;
+// }
+
+// export default db;
---
+++ .history/server_20221117125218.js
@@ -0,0 +1,51 @@
+// import express from "express";
+
+const express = require(express);
+import { join } from "path";
+import fileupload from "express-fileupload";
+
+let initial_path = join(__dirname, "public");
+
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+
+app.get("/", (req, res) => {
+  res.sendFile(join(initial_path, "home.html"));
+});
+
+app.get("/editor", (req, res) => {
+  res.sendFile(join(initial_path, "editor.html"));
+});
+
+// upload link
+app.post("/upload", (req, res) => {
+  let file = req.files.image;
+  let date = new Date();
+  // image name
+  let imagename = date.getDate() + date.getTime() + file.name;
+  // image upload path
+  let path = "public/uploads/" + imagename;
+
+  // create upload
+  file.mv(path, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      // our image upload path
+      res.json(`uploads/${imagename}`);
+    }
+  });
+});
+
+app.get("/:blog", (req, res) => {
+  res.sendFile(join(initial_path, "blog.html"));
+});
+
+app.use((req, res) => {
+  res.json("404");
+});
+
+app.listen("3001", () => {
+  console.log("listening......");
+});
